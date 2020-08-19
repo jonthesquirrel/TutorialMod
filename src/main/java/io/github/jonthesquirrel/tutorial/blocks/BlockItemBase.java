8fe1759208b6306b0a244d3f374ac92568b0ae41
Jonathan Herman <jdh11235@gmail.com>
2020-08-18 22:10:40 -0700

    Complete episode 4

---
+++ src/main/java/io/github/jonthesquirrel/tutorial/blocks/BlockItemBase.java
@@ -0,0 +1,14 @@
+package io.github.jonthesquirrel.tutorial.blocks;
+
+import io.github.jonthesquirrel.tutorial.Tutorial;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+public class BlockItemBase extends BlockItem {
+
+    public BlockItemBase(Block block) {
+        super(block, new Item.Properties().group(Tutorial.TAB));
+    }
+
+}
