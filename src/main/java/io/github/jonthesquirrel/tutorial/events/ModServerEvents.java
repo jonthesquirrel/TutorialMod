c15b5f84c3747116c5afb5b4b7bd450057bcda3f
Jonathan Herman <jdh11235@gmail.com>
2020-09-19 12:57:54 -0700

    Ep 15 events

---
+++ src/main/java/io/github/jonthesquirrel/tutorial/events/ModServerEvents.java
@@ -0,0 +1,21 @@
+package io.github.jonthesquirrel.tutorial.events;
+
+import io.github.jonthesquirrel.tutorial.Tutorial;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(
+        modid = Tutorial.MOD_ID,
+        bus = Mod.EventBusSubscriber.Bus.FORGE,
+        value = Dist.DEDICATED_SERVER
+)
+public class ModServerEvents {
+
+    @SubscribeEvent
+    public static void onServerChat(ServerChatEvent event) {
+
+    }
+
+}
